4d742a6966ececc9831722a4b21d77eb8973b8b1
Gracie Hecha <gracefullsoul19@gmail.com>
2022-11-18 17:21:33 -0500

    added initial code

--- myprogram.py
+++ myprogram.py
@@ -1,1 +1,14 @@
 #Program to sort alphabetically the words from a string provided by the user
+my_str = input ('Enter a string: ')
+
+#breakdown the string into a list of words
+words = [word.lower() for word in my_str.split()]
+
+#sort the list
+words.sort()
+
+#disiplay the sorted words
+print("The sorted words are:")
+
+for word in words:
+    print(word)
