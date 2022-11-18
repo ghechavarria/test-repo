34a82e7ba6163e75cd3dacf4c89da6ce127968d0
Gracie Hecha <gracefullsoul19@gmail.com>
2022-11-18 17:49:40 -0500

    added feature color change

--- myprogram.py
+++ myprogram.py
@@ -1,14 +1,27 @@
+#pip install termcolo
+from termcolor import colored
+
+#define punctuation
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+
 #Program to sort alphabetically the words from a string provided by the user
-my_str = input ('Enter a string: ')
+my_str = "Hello, World. How are you?"
+
+#https://pastebin.com/9xcTchz4
+#remove punctuation
+no_punct = ""
+for char in my_str:
+    if char not in punctuations:
+        no_punct = no_punct + char
 
 #breakdown the string into a list of words
-words = [word.lower() for word in my_str.split()]
+words = [word.lower() for word in no_punct.split()]
 
 #sort the list
 words.sort()
 
 #disiplay the sorted words
 print("The sorted words are:")
-
 for word in words:
-    print(word)
+    print(colored(word, 'blue'))
